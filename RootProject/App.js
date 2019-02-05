f197e75a6d334f12e3fbd4332ed597ef968e786e
Charlotte Driscoll <charlotte.d.driscoll@icloud.com>
2019-02-05 12:20:53 -0600

    Created React Native App

---
+++ RootProject/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>I GOT THIS TO WORK BOOM</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
