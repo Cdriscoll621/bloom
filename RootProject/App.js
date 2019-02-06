17504ab7b908f033481a5baaab059302a1603a8c
Charlotte Driscoll <charlotte.d.driscoll@icloud.com>
2019-02-06 10:04:51 -0600

    Removed Commented Code

--- RootProject/App.js
+++ RootProject/App.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>I GOT THIS TO WORK BOOM</Text>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#eee',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
 import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 import { Font, AppLoading } from 'expo';
